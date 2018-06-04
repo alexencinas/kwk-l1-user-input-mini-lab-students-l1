0f6aa7a289f15fc63faa52e49eb52b77162da8da
alexencinas <shoogiee123@gmail.com>
2018-06-04 22:41:49 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -3,5 +3,11 @@ def greeting
 end
 greeting
 
-def  choice greeting
-  
+def  choice_greeting
+  greeting = "Hello"
+  my_name = "Alex"
+  your_name = "Alice"
+  time_of_day =  "evening"
+  puts "#{greeting} #{your_name}, I'm #{my_name}, how's your #{time_of_day}."
+end
+choice_greeting
